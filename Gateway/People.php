daf743e836841411b43e7019fb9d845258ac2910
Sebastian List <basti@mlist-online.de>
2015-07-15 15:11:00 +0200

    escaped db table names and fields

--- Gateway/People.php
+++ Gateway/People.php
@@ -2,11 +2,11 @@
 class Gateway_People extends Gateway_Base{
 
   public function find( $ids ){
-    $sql = "SELECT people.data->>'id' AS id,
-                   people.data
-            FROM   people
-            WHERE  deleted = FALSE
-              AND  people.data->>'id' IN ";
+    $sql = "SELECT \"people\".\"data\"->>'id' AS \"id\",
+                   \"people\".\"data\"
+            FROM   \"people\"
+            WHERE  \"deleted\" = FALSE
+              AND  \"people\".\"data\"->>'id' IN ";
     $result = $this->findByIds($sql,$ids);
     $people = array();
     foreach ( $result as $row ){
